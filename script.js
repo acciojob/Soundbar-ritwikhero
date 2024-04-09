a9f4e5d40ec612c36679a3ac47d2731fc1317341
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-04-09 22:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function soundPlay(audioPlayer){ 
+let audioPlayer = document.getElementsByClassName("btn");
+audioPlayer.src = "D:\New folder\sound\short-crowd-cheer-6713.mp3"+ soundbar;
+audioPlayer.play();
+}
+btns.addEventListener("click", soundPlay);
+function soundPlay(audioPlayer){ 
+    let audioPlayer = document.getElementsByClassName("btn");
+    audioPlayer.src = "D:\New folder\sound\boo-6377.mp3" + soundbar;
+    audioPlayer.play();
+    }
+    btns.addEventListener("click", soundPlay);
+    
+
+function soundStop(){
+    let audioPlayer = getElementsByClassName("stop");
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
